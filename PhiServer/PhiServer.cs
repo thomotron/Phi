d0261747c64443cc9d57a34b52bee035006b04a4
Thomotron <gigatech55@gmail.com>
2018-07-04 13:45:19 +0930

    Document server methods

--- PhiServer/PhiServer.cs
+++ PhiServer/PhiServer.cs
@@ -20,27 +20,46 @@ namespace PhiServer
             server.Message += OnMessage;
         }
 
+        /// <summary>
+        /// Starts the server.
+        /// </summary>
         public void Start()
         {
             server.Start();
         }
 
+        /// <summary>
+        /// Stops the server.
+        /// </summary>
         public void Stop()
         {
             server.Stop();
             connections.Clear();
         }
 
+        /// <summary>
+        /// Callback for incoming connections.
+        /// </summary>
+        /// <param name="connection"></param>
         private void OnConnect(WebsocketServerConnection connection)
         {
             connections.Add(connection);
         }
 
+        /// <summary>
+        /// Callback for client disconnections.
+        /// </summary>
+        /// <param name="connection"></param>
         private void OnDisconnect(WebsocketServerConnection connection)
         {
             connections.Remove(connection);
         }
 
+        /// <summary>
+        /// Callback for messages received from clients.
+        /// </summary>
+        /// <param name="client">The connection the message originated from</param>
+        /// <param name="data">The message payload</param>
         private void OnMessage(WebsocketServerConnection client, byte[] data)
         {
             throw new NotImplementedException();
