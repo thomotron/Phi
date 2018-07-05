2e00cfb1957ec70261a264a553a70221772af10d
Thomotron <gigatech55@gmail.com>
2018-07-05 19:08:52 +0930

    Add common connection defaults

---
+++ PhiData/ConnectionDefaults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhiClient
+{
+    public static class ConnectionDefaults
+    {
+        public const string DEFAULT_SERVER_ADDRESS = "localhost";
+        public const int DEFAULT_SERVER_PORT = 16180;
+    }
+}
